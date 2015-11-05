81fba165c5af833706b9ad2eef2bb47c05147222
ymyjohnny <ymyjohnny@adsame.com>
2015-11-05 17:46:41 +0800

    avg add

--- python/python学习/day3/avg.py
+++ python/python学习/day3/avg.py
@@ -6,14 +6,26 @@ Created on 2015-11-1
 '''
 
 #写一个函数 算出一堆数字的平均数，并做异常检测
+import sys
 
-def add(*l):
-    s = 0
-    for i in l:
-        s += i 
-    return (s /  len(l))
+def numlist(a,b):
+    list = range(a,b+1)
+    return list
 
-print add(1,2,3,4,5,6,7)
+def main():
+    testa = int(sys.argv[1])
+    testb = int(sys.argv[2])
+    data = numlist(testa,testb)
+    print data
+    sum = 0
+    for i in data:
+        sum += i
+        print sum
+        avg = sum/len(data)
+    print avg
+
+if __name__ == '__main__':
+    main()
  
     
 #if __name__ == '__main__':
